9117fd4eb55be3ec79cadbac82f2eed40c6d9125
Vitya <Utopio@tut.by>
2021-09-21 09:57:36 +0300

    /?

--- src/test/java/by/it_academy/java_enterprise/knyazev/CubeTest.java
+++ src/test/java/by/it_academy/java_enterprise/knyazev/CubeTest.java
@@ -7,22 +7,21 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class CubeTest 
-{
+public class CubeTest {
 	private Cube cube = new Cube();
-    
-    @Test
-    public void testPositiveResultOnGetCube() {
-    	int value = 5;
-    	int result = cube.getCube(value);
-    	assertEquals(125, result);
-    }
-    
-    @Test
-    public void testNegativeResultOnGetCube() {
-    	int value = -5;
-    	int result = cube.getCube(value);
-    	assertEquals(-125, result);
-    }
-    
+
+	@Test
+	public void testPositiveResultOnGetCube() {
+		int value = 5;
+		int result = cube.getCube(value);
+		assertEquals(125, result);
+	}
+
+	@Test
+	public void testNegativeResultOnGetCube() {
+		int value = -5;
+		int result = cube.getCube(value);
+		assertEquals(-125, result);
+	}
+
 }
